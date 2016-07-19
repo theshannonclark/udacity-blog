5f44fb721a35304a74758954a3b7d01b3064d4b2
Shannon Clark <theshannonclark@gmail.com>
2016-07-19 21:22:06 +1000

    Convert salt to hexadecimal

--- app/auth.py
+++ app/auth.py
@@ -1,7 +1,9 @@
 import os
 import random
 import hashlib
+import binascii
 import hmac
+import logging
 
 
 secret = "'KT?|S$)[XtoD|&efbpWBV4-mw59x}QwWVt9Pa7u01c&~<&A`)~)2M]-FJ8A5-ja*');"
@@ -15,12 +17,13 @@ def check_secure_val(secure_val):
         return val
 
 def make_salt(length = 16):
-    return os.urandom(length)
+    return binascii.hexlify(os.urandom(16))
 
 def make_pw_hash(name, pw, salt = None):
     if not salt:
         salt = make_salt()
     h = hashlib.sha256(name + pw + salt).hexdigest()
+    logging.error("HASH: %s" % h)
     return "%s,%s" % (salt, h)
 
 def valid_password(name, password, h):
